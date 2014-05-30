2d3dd2537a23a95fc2b760cf82df09512931f889
Brad Gearon <bradgearon@gmail.com>
2014-05-29 22:26:09 -0500

    removed (duh) bootstrap-windows...

--- script/bootstrap.cmd
+++ script/bootstrap.cmd
@@ -1,3 +1,7 @@
+call grab-cache.cmd
+set cacheDir=node_modules
+for /f "delims=;" %%f in ('dir /b %cacheDir%') do npm cache add %cacheDir%\%%f
+
 @IF EXIST "%~dp0\node.exe" (
   "%~dp0\node.exe"  "%~dp0\bootstrap" %*
 ) ELSE (
--- script/grab-cache.cmd
+++ script/grab-cache.cmd
@@ -3,4 +3,3 @@ set cacheTarget=npm-cache.7z
 set cacheLocation=https://www.dropbox.com/s/6pbuzkl5a61at9y/npm-cache.7z
 wget --no-check-certificate %cacheLocation% -O %cacheTarget%
 7za -y x %cacheTarget%
-for /f "delims=;" %%f in ('dir /b %cacheDir%') do npm cache add %cacheDir%\%%f
