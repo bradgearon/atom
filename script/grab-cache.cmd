dced1f258695607659eacf8900c71d7956aa13ed
Brad Gearon <bradgearon@gmail.com>
2014-06-04 03:40:47 -0500

    junk!

--- script/grab-cache.cmd
+++ script/grab-cache.cmd
@@ -1,6 +1,6 @@
 set cacheDir=node_modules
 set cacheTarget=npm-cache.7z
 set cacheLocation=https://www.dropbox.com/s/6pbuzkl5a61at9y/npm-cache.7z
-wget --no-check-certificate %cacheLocation% -O %cacheTarget%
+call wget --no-check-certificate %cacheLocation% -O %cacheTarget%
 call 7za -y x %cacheTarget%
 for /f "delims=;" %%f in ('dir /b %cacheDir%') do npm cache add node_modules\%%f
