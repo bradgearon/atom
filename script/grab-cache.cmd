be4e68e85b76747be7749fa8c218db76ca3fc65a
Brad Gearon <bradgearon@gmail.com>
2014-06-05 02:02:53 -0500

    perhaps manual npm install will help

--- script/grab-cache.cmd
+++ script/grab-cache.cmd
@@ -1,6 +1,9 @@
-set cacheDir=node_modules
-set cacheTarget=npm-cache.7z
-set cacheLocation=https://www.dropbox.com/s/6pbuzkl5a61at9y/npm-cache.7z
-call wget --no-check-certificate %cacheLocation% -O %cacheTarget%
-call 7za -y x %cacheTarget%
-for /f "delims=;" %%f in ('dir /b %cacheDir%') do npm cache add node_modules\%%f
+set NODE_ENV=production
+set npm_config_ignore-scripts=true
+call npm install --target=0.11.10 --msvs_version=2010 --msvs_arch=x86
+REM set cacheDir=node_modules
+REM set cacheTarget=npm-cache.7z
+REM set cacheLocation=https://www.dropbox.com/s/6pbuzkl5a61at9y/npm-cache.7z
+REM call wget --no-check-certificate %cacheLocation% -O %cacheTarget%
+REM call 7za -y x %cacheTarget%
+REM for /f "delims=;" %%f in ('dir /b %cacheDir%') do npm cache add node_modules\%%f
